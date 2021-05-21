ac5c9876484c9d95ccc58922e5d9cd1772007635
fazzyA <faz.pak@gmail.com>
2021-05-21 15:08:48 +0500

    remove usr

--- users/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken = (id, isAdmin, name) => {
-  return jwt.sign({ id, isAdmin, name }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-}
-
-export default generateToken
